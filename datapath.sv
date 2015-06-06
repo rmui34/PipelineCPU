d3577f028401332dc9a0c8b30dc1878f4f74f493
Beck-Sisyphus <pshfls@gmail.com>
2015-06-05 23:41:42 -0700

    adding forward

--- datapath.sv
+++ datapath.sv
@@ -22,47 +22,52 @@ module datapath (
 	wire [4:0] 	RsID, RtID, RdID, write,
 				RsEX, RtEX, RdEX,
 				RdRtEX, RdRtME, RdRtWB;
-
+	wire [1:0] ForwardA, ForwardB;
 	reg [2:0] ALUctrl;
 	reg zero, overflow, carryout, negative, negativeReg;
 	wire ALUSrc, RegDst, MemRdEn, MemWrEn, RegWrEn;
 	wire [1:0] ALUOp;
-	assign RsID = instr[25:21];
-	assign RtID = instr[20:16];
-	assign RdID = instr[15:11];
+	assign 		RsID = instr[25:21];
+	assign 		RtID = instr[20:16];
+	assign 		RdID = instr[15:11];
 
 	// Need to put register file, ID/EX registers, ALU, 
 	// EX/MEM register, Data Memory, MEM/WB register, and mux here
 
 	// Instruction decoding state
 	registerFile file(clk, RsID, RtID, write, RegWrEn, busWB, busA, busB);
-	assign busC 	= {{16{instr[15]}}, instr[15:0]};
+	assign 		busC 	= {{16{instr[15]}}, instr[15:0]};
 	IDEXReg  	IDEX (clk, 	RsID, RtID, RdID, WBID, MEID, EXID, busA, busB, busC, 
 							RsEX, RtEX, RdEX, WBEX, MEEX, EXEX, busD, busE, busF);
 
 	// Execution, address calculation state
-	assign ALUSrc	= EXEX[0];
-	assign ALUOp 	= EXEX[2:1];
-	assign RegDst	= EXEX[3];
-	assign busS		= ALUSrc ? busF : busE; // if EXID is 1, choose immediate busF, otherwise choose register output
+	assign 		ALUSrc	= EXEX[0];
+	assign 		ALUOp 	= EXEX[2:1];
+	assign 		RegDst	= EXEX[3];
+			// add forwarding inside
+	forwardUnit	forward(RsEX, RtEX, RdRtME, RdRtWB, WBME[1], WBWB[1], ForwardA, ForwardB);
+	// Need to build a 32 bits mux
+	mux4_1 		ALUA (busX, busD, busWB, busALUb, 32'bx, ForwardA[1], ForwardA[0]);
+	mux4_1		ALUB (busX, busE, busWB, busALUb, 32'bx, ForwardB[1], ForwardB[0]);
+
+	assign 		busS	= ALUSrc ? busF : busE; // if EXID is 1, choose immediate busF, otherwise choose register output
 	ALUcontrol	translate (.ALUop(ALUOp), .instr(instr[5:0]), .ALUin(ALUctrl));
-	assign RdRtEX 	= RegDst ? RtEX : RdEX;
 	ALUnit  	ALU  (ALUctrl, busD, busS, busALUa, zero, overflow, carryout, negative);
+	assign 		RdRtEX 	= RegDst ? RtEX : RdEX;
 	EXMEMReg 	EXMEM(clk, 	WBEX, MEEX, busE, busALUa, RdRtEX, 
 							WBME, MEME, busG, busALUb, RdRtME);
 
 	// Memory Access state
-			// assign Branch	= MEME[2] & ~negativeReg & ~zeroReg;	// Branch greater than at not negative nor zero
-	assign MemRdEn	= MEME[1];
-	assign MemWrEn	= MEME[0]; // MemRdEn was not implemented so MEME[1] was not used
+	assign 		MemRdEn	= MEME[1];
+	assign 		MemWrEn	= MEME[0]; // MemRdEn was not implemented so MEME[1] was not used
 	dataMemory 	data (.clk(clk), .rst(MemRst), .adx(busALUb), .WrEn(MemWrEn), .data(busData));
-	assign busData 	= MemWrEn ? 16'bZ : busG[15:0]; // When store word, the data comes from the register file
-	assign busH 	= {{16{busData[15]}}, busData};    
+	assign 		busData = MemWrEn ? 16'bZ : busG[15:0]; // When store word, the data comes from the register file
+	assign 		busH 	= {{16{busData[15]}}, busData};    
 	MEMWBReg 	MEMWB(clk,	WBME,RdRtME, busH, busALUb, 
 							WBWB,RdRtWB, busI, busALUc);
 
 	// Write back state
-	assign busWB	= WBWB[0] ? busALUc : busI; // Write back mux; If WBsel is 0, write back ALU result; if 1, write from memory
-	assign RegWrEn  = WBWB[1];
-	assign write 	= RdRtWB;
+	assign 		busWB	= WBWB[0] ? busALUc : busI; // Write back mux; If WBsel is 0, write back ALU result; if 1, write from memory
+	assign		RegWrEn = WBWB[1];
+	assign 		write 	= RdRtWB;
 endmodule
--- forwardUnit.v
+++ forwardUnit.v
@@ -1,13 +1,13 @@
 // Forwarding Unit for pipelining CPU
 module forwardUnit (
-	input clk,    // Clock
+	// input clk,    // Clock
 	input [4:0] RsEX, RtEX, RdRtME, RdRtWB,
 	input RegWriteME, RegWriteWB,
 	output [1:0] ForwardA, ForwardB
 );
 
 	always @(*) begin
-
+		// Data Hazard
 	end
 
 endmodule
