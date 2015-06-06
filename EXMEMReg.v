593583009853d63a0b8e467e2319161ee98ebedb
Beck-Sisyphus <pshfls@gmail.com>
2015-06-05 19:31:34 -0700

    Finish the datapath with everything go through the register, except a shifter and an adder in 4.51

--- EXMEMReg.v
+++ EXMEMReg.v
@@ -1,16 +1,18 @@
 //Raymond Mui , AJ Townsend, Beck Pang
-module EXMEMReg(clk,WB, M, DataIn,ALURes,RdRt,
-  WBReg, MReg, DataOut,ALUReg, RdRtReg);
+module EXMEMReg(clk,WB, M, DataIn,ALURes,RdRt,zero
+  WBReg, MReg, DataOut,ALUReg, RdRtReg, zeroReg);
   input clk;
   input [1:0] WB;
   input [2:0] M;
   input [4:0] RdRt;
   input [31:0] DataIn,ALURes;
+  input zero;
 
   output reg [1:0] WBReg;
   output reg [2:0] MReg;
   output reg [4:0] RdRtReg;
   output reg [31:0] DataOut,ALUReg;
+  output reg zeroReg;
 
   initial begin
     WBReg = 0;
@@ -18,6 +20,7 @@ module EXMEMReg(clk,WB, M, DataIn,ALURes,RdRt,
     ALUReg = 0;
     DataOut = 0;
     RdRtReg = 0;
+    zeroReg = 0;
   end
 
   always @(posedge clk)
@@ -27,6 +30,7 @@ module EXMEMReg(clk,WB, M, DataIn,ALURes,RdRt,
     ALUReg <= ALURes;
     DataOut <= DataIn;
     RdRtReg <= RdRt;
+    zeroReg <= zero;
   end
 
 endmodule
--- datapath.sv
+++ datapath.sv
@@ -1,13 +1,13 @@
 module datapath (
-	input clk,    // Clock
+	input clk,  	// Clock
+	input MemRst,	// Load memory from a txt file
 	input [4:0] read0, read1, write,
-
 	// control lines
 	input [1:0] WBID,	// RegWrite, MemtoReg
-	input [2:0] MID,	// Branch, MemRead, MemWrite
+	input [2:0] MEID,	// Branch, MemRead, MemWrite
 	input [3:0] EXID,	// RegDst, ALUOp1, ALUOp0, ALUSrc
-	input [15:0] instr,
-	input RegWrEn, MemWrEn
+	input [31:0] instr,
+	output zeroReg, Branch
 );
 	
 	wire [31:0] busA, busB, busC, busD, busE, busF,
@@ -16,43 +16,53 @@ module datapath (
 				busH, busI		// Memory Output
 				busWB; 			// page 309
 	
-	wire [1:0] WBEX, WBMEM, WBWB;
-	wire [2:0] MEX,  MMEM;
+	wire [1:0] WBEX, WBME, WBWB;
+	wire [2:0] MEEX,  MEME;
 	wire [3:0] EXEX;
-	reg [4:0] Rs, Rt, Rd, RdRt;
-	reg MemRst;
-	reg WBsel;
-
-	wire [4:0] RsReg,RtReg,RdReg, RdRtReg;
+	wire [4:0] 	RsID, RtID, RdID, write,
+				RsEX, RtEX, RdEX,
+				RdRtEX, RdRtME, RdRtWB;
 
 	reg [2:0] ALUctrl;
-	reg zero, overflow, carryout, negative;
-
+	reg zero, overflow, carryout, negative, zeroReg;
+	wire ALUSrc, RegDst, MemRdEn, MemWrEn, RegWrEn;
+	wire [1:0] ALUOp;
+	assign RsID = instr[25:21];
+	assign RtID = instr[20:16];
+	assign RdID = instr[15:11];
 
 	// Need to put register file, ID/EX registers, ALU, 
 	// EX/MEM register, Data Memory, MEM/WB register, and mux here
 
 	// Instruction decoding state
-	registerFile file(clk, read0, read1, write, RegWrEn, busWB, busA, busB);
-	assign busC 	= {{16{instr[15]}}, instr};
-	// assign busB  	= EXID
-	IDEXReg  	IDEX (clk, Rs, Rt, Rd, WBID, MID, EXID, busA, busB, busC, RsReg, RtReg, 
-								RdReg, WBEX, MEX, EXEX, busD, busE, busF);
+	registerFile file(clk, RsID, RtID, write, RegWrEn, busWB, busA, busB);
+	assign busC 	= {{16{instr[15]}}, instr[15:0]};
+	IDEXReg  	IDEX (clk, 	RsID, RtID, RdID, WBID, MEID, EXID, busA, busB, busC, 
+							RsEX, RtEX, RdEX, WBEX, MEEX, EXEX, busD, busE, busF);
 
 	// Execution, address calculation state
-	assign busS		= EXID[0] ? busF : busE; // if EXID is 1, choose immediate busF, otherwise choose register output
+	assign ALUSrc	= EXEX[0];
+	assign ALUOp 	= EXEX[2:1];
+	assign RegDst	= EXEX[3];
+	assign busS		= ALUSrc ? busF : busE; // if EXID is 1, choose immediate busF, otherwise choose register output
+	ALUcontrol	translate (.ALUop(ALUOp), .instr(instr[5:0]), .ALUin(ALUctrl));
+	assign RdRtEX 	= RegDst ? RtEX : RdEX;
 	ALUnit  	ALU  (ALUctrl, busD, busS, busALUa, zero, overflow, carryout, negative);
-	EXMEMReg 	EXMEM(clk, WBEX, MEX, busD, busALUa, RdRt,
-						 WBMEM, MMEM, busALUb, busG, RdRtReg);
+	EXMEMReg 	EXMEM(clk, 	WBEX, MEEX, busE, busALUa, RdRtEX, zero
+							WBME, MEME, busG, busALUb, RdRtME, zeroReg);
 
 	// Memory Access state
+	assign Branch	= MEME[2];
+	assign MemRdEn	= MEME[1];
+	assign MemWrEn	= MEME[0]; // MemRdEn was not implemented so MEME[1] was not used
 	dataMemory 	data (.clk(clk), .rst(MemRst), .adx(busALUb), .WrEn(MemWrEn), .data(busData));
 	assign busData 	= MemWrEn ? 16'bZ : busG[15:0]; // When store word, the data comes from the register file
 	assign busH 	= {{16{busData[15]}}, busData};    
-	MEMWBReg 	MEMWB(clk,WBMEM,Rd, busH, busALUb, WBWB,RdReg,busI,busALUc);
-	assign busWB	= WBsel ? busALUc : busI; // Write back mux; If WBsel is 0, write back ALU result; if 1, write from memory
+	MEMWBReg 	MEMWB(clk,	WBME,RdRtME, busH, busALUb, 
+							WBWB,RdRtWB, busI, busALUc);
 
-	// Wire up the control lines
-	ALUcontrol	translate (.ALUop(EXID[2:1]), .instr(instr[5:0]), .ALUin(ALUctrl));
-
+	// Write back state
+	assign busWB	= WBWB[0] ? busALUc : busI; // Write back mux; If WBsel is 0, write back ALU result; if 1, write from memory
+	assign RegWrEn  = WBWB[1];
+	assign write 	= RdRtWB;
 endmodule
