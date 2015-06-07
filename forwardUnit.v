66ad699330de53c606a720b47c682947e7a01ac8
rmui34 <r.mui34@gmail.com>
2015-06-06 17:27:47 -0700

    finished logic for forwarding unit and some additional comments in hazardDetect

--- forwardUnit.v
+++ forwardUnit.v
@@ -1,13 +1,32 @@
 // Forwarding Unit for pipelining CPU
-module forwardUnit (
-	// input clk,    // Clock
-	input [4:0] RsEX, RtEX, RdRtME, RdRtWB,
-	input RegWriteME, RegWriteWB,
-	output [1:0] ForwardA, ForwardB
-);
+module forwardUnit (IDEXRs, IDEXRt, EXMEMRdMEM, MEMWBRd, EXMEMRegWrite, MEMWBRegWrite,ForwardA, ForwardB);
+	input [4:0] IDEXRs, IDEXRt, EXMEMRd, MEMWBRd;
+	input EXMEMRegWrite, MEMWBRegWrite;
+	output reg [1:0] ForwardA, ForwardB
 
-	always @(*) begin
-		// Data Hazard
-	end
+
+	//Forward A
+	always @(EXMEMRegWrite or MEMWBRegWrite or EXMEMRd or MEMWBRd or IDEXRs)
+		begin
+			if (EXMEMRegWrite && (EXMEMRd != 0) && (EXMEMRd == IDEXRs))
+				ForwardA = 2'b10;
+			else if(MEMWBRegWrite && (MEMWBRd != 0) &&  ~(EXMEMRegWrite & (EXMEMRd != 0)) && (EXMEMRd !=IDEXRs) && (MEMWBRd == IDEXRs))
+				ForwardB = 2'b01;
+			else
+				ForwardA = 2'b00;
+		end
+
+
+	//Forward B
+	always @(EXMEMRegWrite or MEMWBRegWrite or EXMEMRd or MEMWBRd or IDEXRt)
+		begin
+			if(EXMEMRegWrite && (EXMEMRd != 0) && (EXMEMRd == IDEXRt))
+				ForwardB = 2'b10;
+			else if (MEMWBRegWrite && (MEMWBRd != 0) &&  ~(EXMEMRegWrite & (EXMEMRd != 0)) && (EXMEMRd !=IDEXRt) && (MEMWBRd == IDEXRt))
+				ForwardB = 2'b01;
+			else
+				ForwardB = 2'b00;
+
+		end
 
 endmodule
--- hazardDetectU.v
+++ hazardDetectU.v
@@ -1,3 +1,9 @@
+// Checking for load instructions, the control for the hazard detection unit is this single condition.
+
+// The first line tests to see if the instruction is a load: the only instruction that reads data memory is a load.
+// The next two lines check to see if the destination register field of the load in the EX stage matches either source register
+// of the instruction in the ID stage. If the condition holds, the instruction stalls one clock cycle. After this 1-cycle stall,
+//  the forwarding logic can handle the dependence and execution proceeds.
 module hazardDetectU(IFIDRegRs,IFIDRegRt,IDEXRegRt,IDEXMemRead,IFIDWr,PCWr,hazCtrl)
   input [4:0]IFIDRegRs,IFIDRegRt,IDEXRegRt;
   input IDEXMemRead;
@@ -15,7 +21,7 @@ module hazardDetectU(IFIDRegRs,IFIDRegRt,IDEXRegRt,IDEXMemRead,IFIDWr,PCWr,hazCt
       begin
         IFIDWr = 1;
         PCWr = 1;
-        hazCtrl= 0;
+        hazCtrl= 1;
       end
 
 endmodule
