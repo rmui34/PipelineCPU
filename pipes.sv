9d2ff01eee56b2ad20bc77c1f619dac77582a2f9
unknown <towaj@EE-365PC10.ee.washington.edu>
2015-06-07 21:47:10 -0700

    clean up

--- pipes.sv
+++ pipes.sv
@@ -1,7 +1,20 @@
 module pipes (clk, rst);
 	input clk, rst;
 	
-	assign jAdx = (JR) ? IFbusA : instrQ[25:0];
+	wire [31:0] jAdx, JRAdx, brAdx, instrD, instrQ, IDbusA, IDbusB;
+	wire [6:0] prgCD, prgCQ;
+	wire [4:0] RsID, RtID, RdID, RsEX, RtEX, RdEX;
+	wire [3:0] IDEX, EX;
+	wire [1:0] ALUop, IDWB, WBEX, WBME, WB;
+	wire IFIDWr, PCWr, hazCtrl, flush, JR, regWR, RegDst, ALUsrc, MemWrEn, Mem2Reg, br, j, IDM, MEX, M, zero;
+	
+	
+	assign flush = 0;
+	assign jAdx = instrQ[25:0];
+	assign JRAdx = IFbusA;
+	assign brAdx = {{16{instrQ[15]}}, instrQ[15:0]};
+	
+	hazardDetectU hazItAll(instrQ[25:21], instrQ[20:16], RtEX, M, IFIDWr, PCWr, hazCtrl);
 	
 	///////////////////////////////////////////////////////////////////////////////////
 	// FETCH																									//
@@ -11,112 +24,70 @@ module pipes (clk, rst);
 	// Stores and Updates the program counter
 	// Takes zero flag and br signal from the decoder from ALU to operate
 	// on the SLT, BEQ pseudoinstruction for BGT
-	ProgCountReg pc(clk, rst, jAdx, brAdx, j, br, zero, prgCount);
+	ProgCountReg pc(clk, rst, jAdx, JRAdx, brAdx, j, JR, br, zero, prgCD, PCWr);
 	
 	// Instruction Memory
 	// Holds the instructions
-	InstruMemory memInstr(clk, prgCount, IMR, instrD, rst);
+	InstruMemory memInstr(clk, prgCD, 1, instrD, rst);
 	
 	// Bridges the Fetch and Decode
-	IFIDReg (clk, flush, IFIDWr, instrD, progCD, progCQ, instrQ);
+	IFIDReg IFID (clk, flush, IFIDWr, instrD, prgCD, prgCQ, instrQ);
 	
 	///////////////////////////////////////////////////////////////////////////////////
 	// DECODE																								//
 	///////////////////////////////////////////////////////////////////////////////////
 	
-	registerFile regs(clk, instrQ[25:21], instrQ[20:16], dstReg, WBregWR, writeData, IFbusA, IFbusB);
+	registerFile regs(clk, instrQ[25:21], instrQ[20:16], dstReg, WB[0], writeData, IDbusA, IDbusB);
 	
 	
 	// Instruction Decoder
 	// Decodes the 32-bit instruction into appropriate control signals
 	InstrucDecoder decoder(instrQ[31:26], j, br, Mem2Reg, ALUop, MemWrEn, ALUsrc, RegDst, regWR, JR);
 	
-	IDEXReg IDEX (clk, RsID, RtID, RdID, {Mem2Reg, regWR}, {br, MemWrEn}, {ALUop, ALUsrc, RegDst}, IFbusA, IFbusB, IFimd, 
-							 RsEX, RtEX, RdEX, WBEX, MEMEX, EX, EXbusA, EXbusB, EXimd);
+	assign {IDWB, IDM, IDEX} = hazCtrl ? 0 : {{Mem2Reg, regWR}, MemWrEn, {ALUop, ALUsrc, RegDst}};
+	
+	assign RsID  = instrQ[25:21];
+	assign RtID  = instrQ[20:16];
+	assign RdID  = instrQ[15:11];
+	assign IDimd = instrQ[15:0];
+	
+	IDEXReg IDEXReg (clk, RsID, RtID, RdID, IDWB, IDM, IDEX, IDbusA, IDbusB, IDimd, 
+							 RsEX, RtEX, RdEX, WBEX, MEX, EX,   EXbusA, EXbusB, EXimd);
 									
 	///////////////////////////////////////////////////////////////////////////////////
 	// EXECUTE																								//
 	///////////////////////////////////////////////////////////////////////////////////
 	
-	assign ALUB = (ALUsrc) ? EXimd : EXbusB;
+	assign ALUB = (ALUsrc) ? EXimd : busB;
+	assign RdRtEX = EX[0] ? RdEX : RtEX;
+	
+	
+	mux32bit4_1 busAfwd(busA, EXbusA, writeData, busALU, 0, fwdA);
+	mux32bit4_1 busBfwd(busB, EXbusB, writeData, busALU, 0, fwdB);
+	
+	forwardUnit movinFWD(RsEX, RtEX, RdRtME, dstReg, WBME[0], WB[0], fwdA, fwdB);
 	
 	ALUcontrol translate(EX[1:0], EXimd[5:0], control);
 	
-	ALUnit logicUnit(control, EXbusA, ALUB, EXbusOut, zero, overflow, carryout, negative);
+	ALUnit logicUnit(control, busA, ALUB, EXbusOut, zero, overflow, carryout, negative);
 	
-	EXMEMReg EXMEM(clk, 	WBEX, MEMEX, EXbusB, EXbusOut, RdRtEX, WBME, ME, busG, busALU, RdRtME);
+	EXMEMReg EXMEM(clk, WBEX, MEX, busB, EXbusOut, RdRtEX, WBME, ME, WrData, busALU, RdRtME);
 	
 	///////////////////////////////////////////////////////////////////////////////////
 	// MEMORY																								//
 	///////////////////////////////////////////////////////////////////////////////////
 	
-	SRAM2Kby16 dataMemory(clk, adxData, MemWrEn, data);
+	assign data = M ? WrData[15:0] : 16'bZ;
 	
-	MEMWBReg MEMWB(clk,	WBME,RdRtME, busH, busALUb, WB,RdRtWB, busI, busALUc);
+	SRAM2Kby16 dataMemory(clk, busALU, M, data);
+	
+	MEMWBReg MEMWB(clk, WBME, RdRtME, data, busALU, WB, dstReg, ReadData, busALUWB);
 	
 	///////////////////////////////////////////////////////////////////////////////////
 	// WRITE BACK																							//
 	///////////////////////////////////////////////////////////////////////////////////
 	
-	/*
-	wire [31:0] busA, busB, busC, busD, busE, busF, busX, busY,
-				busS, busALUa, busALUb, busALUc,	// ALU result
-				busG, busData, 	// Memory Input
-				busH, busI,		// Memory Output
-				busWB; 			// page 309
+	assign writeData = WB[1] ? ReadData : busALUWB;
+
+	endmodule
 	
-	wire [1:0] WBEX, WBME, WBWB;
-	wire [2:0] MEEX,  MEME;
-	wire [3:0] EXEX;
-	wire [4:0] 	RsID, RtID, RdID, write,
-				RsEX, RtEX, RdEX,
-				RdRtEX, RdRtME, RdRtWB;
-	wire [1:0] ForwardA, ForwardB;
-	wire [2:0] ALUctrl;
-	wire zero, overflow, carryout, negative, negativeReg;
-	wire ALUSrc, RegDst, MemRdEn, MemWrEn, RegWrEn;
-	wire [1:0] ALUOp;
-	assign 		RsID = instr[25:21];
-	assign 		RtID = instr[20:16];
-	assign 		RdID = instr[15:11];
-
-	// Need to put register file, ID/EX registers, ALU, 
-	// EX/MEM register, Data Memory, MEM/WB register, and mux here
-/*
-	// Instruction decoding state
-	registerFile file(clk, RsID, RtID, write, RegWrEn, busWB, busA, busB);
-	assign 		busC 	= {{16{instr[15]}}, instr[15:0]};
-	IDEXReg  	IDEX (clk, 	RsID, RtID, RdID, WBID, MEID, EXID, busA, busB, busC, 
-							RsEX, RtEX, RdEX, WBEX, MEEX, EXEX, busD, busE, busF);
-
-	// Execution, address calculation state
-	assign 		ALUSrc	= EXEX[0];
-	assign 		ALUOp 	= EXEX[2:1];
-	assign 		RegDst	= EXEX[3];
-			// add forwarding inside
-	forwardUnit	forward(RsEX, RtEX, RdRtME, RdRtWB, WBME[1], WBWB[1], ForwardA, ForwardB);
-	// Need to build a 32 bits mux
-	mux32bit4_1 ALUA (busX, busD, busWB, busALUb, 32'bx, ForwardA);
-	mux32bit4_1 ALUB (busY, busE, busWB, busALUb, 32'bx, ForwardB); // Figure 4.57
-
-	assign 		busS	= ALUSrc ? busF : busY; // if EXID is 1, choose immediate busF, otherwise choose register output
-	ALUcontrol	translate (.ALUop(ALUOp), .instr(instr[5:0]), .ALUin(ALUctrl));
-	ALUnit  	ALU  (ALUctrl, busX, busS, busALUa, zero, overflow, carryout, negative);
-	assign 		RdRtEX 	= RegDst ? RtEX : RdEX;
-	EXMEMReg 	EXMEM(clk, 	WBEX, MEEX, busY, busALUa, RdRtEX, 
-							WBME, MEME, busG, busALUb, RdRtME);
-
-	// Memory Access state
-	assign 		MemRdEn	= MEME[1];
-	assign 		MemWrEn	= MEME[0]; // MemRdEn was not implemented so MEME[1] was not used
-	dataMemory 	data (.clk(clk), .rst(MemRst), .adx(busALUb), .WrEn(MemWrEn), .data(busData));
-	assign 		busData = MemWrEn ? 16'bZ : busG[15:0]; // When store word, the data comes from the register file
-	assign 		busH 	= {{16{busData[15]}}, busData};    
-	MEMWBReg 	MEMWB(clk,	WBME,RdRtME, busH, busALUb, 
-							WBWB,RdRtWB, busI, busALUc);
-
-	// Write back state
-	assign 		busWB	= WBWB[0] ? busALUc : busI; // Write back mux; If WBsel is 0, write back ALU result; if 1, write from memory
-	assign		RegWrEn = WBWB[1];
-	assign 		write 	= RdRtWB;
-endmodule
